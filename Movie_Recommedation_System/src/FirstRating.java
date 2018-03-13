8bb405dc7e14e3497e168c22dc952f199c19f1de
zzh2616 <zzh2616@hotmail.com>
2018-03-13 14:05:17 +0100

    initial commit

---
+++ Movie_Recommedation_System/src/FirstRating.java
@@ -0,0 +1,12 @@
+import edu.duke.*;
+import java.util.*;
+import org.apache.commons.csv.*;
+
+public class FirstRating {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
